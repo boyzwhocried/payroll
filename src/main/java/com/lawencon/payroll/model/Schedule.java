4249092deaaed891e7f7b2cd712aef37b1f3fa64
benDaMint <billyyong30@gmail.com>
2024-05-14 10:54:54 +0700

    added schedule model

---
+++ src/main/java/com/lawencon/payroll/model/Schedule.java
@@ -0,0 +1,28 @@
+package com.lawencon.payroll.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "t_r_schedule")
+public class Schedule extends BaseModel {
+
+    @ManyToOne
+    @Column(name = "client_assignment_id", nullable = false)
+    private ClientAssignment clientAssignmentId;
+
+    @Column(name = "payroll_date", nullable = false)
+    private LocalDateTime payrollDate;
+
+    @Column(name = "last_file_upload_date", nullable = false)
+    private LocalDateTime lastFileUploadDate;
+}
