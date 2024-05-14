d6c5493017e5a898b1dcb5fa43cd30a699d72ffd
benDaMint <billyyong30@gmail.com>
2024-05-14 10:36:52 +0700

    added document model

--- src/main/java/com/lawencon/payroll/model/Document.java
+++ src/main/java/com/lawencon/payroll/model/Document.java
@@ -1,5 +1,6 @@
 package com.lawencon.payroll.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -11,11 +12,19 @@ import lombok.Setter;
 @Getter
 @Entity
 @Table(name = "t_r_document")
-public class Document {
+public class Document extends BaseModel {
 
     @ManyToOne
+    @Column(name = "file_id", nullable = false)
     private File fileId;
+
+    @ManyToOne
+    @Column(name = "document_type_id", nullable = false)
     private DocumentType documentTypeId;
+
+    @Column(name = "is_signed_by_sender", nullable = false)
     private Boolean isSignedBySender;
+
+    @Column(name = "is_signed_by_receiver", nullable = false)
     private Boolean isSignedByReceiver;
 }
