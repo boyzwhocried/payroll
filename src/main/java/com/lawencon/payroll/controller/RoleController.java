098c7b7ee125c6747aaa99de0c9289330cc839a6
benDaMint <billyyong30@gmail.com>
2024-05-15 11:02:13 +0700

    Added : RoleController, RoleService, RoleServiceImpl, RoleResDto, RoleRepository(findByRoleCodeNot(String code)

---
+++ src/main/java/com/lawencon/payroll/controller/RoleController.java
@@ -0,0 +1,28 @@
+package com.lawencon.payroll.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lawencon.payroll.dto.role.RoleResDto;
+import com.lawencon.payroll.service.RoleService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("roles")
+@RequiredArgsConstructor
+public class RoleController {
+
+    private final RoleService roleService;
+
+    @GetMapping("")
+    public ResponseEntity<List<RoleResDto>> getUserCreationRoles() {
+        final List<RoleResDto> roles = roleService.getRolesExcept();
+        return new ResponseEntity<>(roles, HttpStatus.OK);
+    }
+}
--- src/main/java/com/lawencon/payroll/repository/RoleRepository.java
+++ src/main/java/com/lawencon/payroll/repository/RoleRepository.java
@@ -1,5 +1,7 @@
 package com.lawencon.payroll.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.lawencon.payroll.model.Role;
@@ -7,4 +9,7 @@ import com.lawencon.payroll.model.Role;
 @Repository
 public interface RoleRepository extends BaseRepository<Role, String>
 {
+    Role findByRoleCode(String code);
+
+    List<Role> findByRoleCodeNot(String code);
 }
