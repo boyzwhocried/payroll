c8617a276e27eff61c64d95a3fc3e5c91e0a0fa3
benDaMint <billyyong30@gmail.com>
2024-05-17 14:37:28 +0700

    Updated : Added formatter on DateUtil.toLocalDateTime

--- src/main/java/com/lawencon/payroll/util/DateUtil.java
+++ src/main/java/com/lawencon/payroll/util/DateUtil.java
@@ -1,10 +1,12 @@
 package com.lawencon.payroll.util;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
     public static LocalDateTime toLocalDateTime(String time) {
-		
-		return LocalDateTime.parse(time);
+		final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+		return LocalDateTime.parse(time, formatter);
 	}
 }
